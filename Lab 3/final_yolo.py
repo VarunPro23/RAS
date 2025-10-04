54f57f4739a7ec1e91db3fe71cfbd1d43d61b21c
Varun <imvarun2023@gmail.com>
2025-10-04 08:38:20 -0700

    small edits

--- Lab 3/final_yolo.py
+++ Lab 3/final_yolo.py
@@ -4,21 +4,20 @@ from ultralytics import YOLO
 from pydobot import Dobot
 
 # Find working camera
-def find_camera():
-    for i in range(3):
-        for backend in [None, cv2.CAP_GSTREAMER, cv2.CAP_FFMPEG]:
-            cap = cv2.VideoCapture(i) if backend is None else cv2.VideoCapture(i, backend)
-            if cap.isOpened() and cap.read()[0]:
-                print(f"Camera found at index {i}")
-                return cap
-            cap.release()
-    raise RuntimeError("No camera found")
+# def find_camera():
+#     cap = cv2.VideoCapture('/dev/video4')
+#     if cap.isOpened() and cap.read()[0]:
+#         # print(f"Camera found at index {i}")
+#         return cap
+#     cap.release()
+
+cap = cv2.VideoCapture('/dev/video0')
 
 # Coordinates
-PICKUP_POS = (240, 5, -42)
-BASKET_A = (250, 143, 75)  # Food basket
-BASKET_B = (-10, 143, 75)  # Vehicle basket
-HOME_POS = (240, -4, 34, 0)  # Home position
+PICKUP_POS = (250, 12.03, -40.00)
+BASKET_A = (258.29, -213.49, 41.72)  # Food basket
+BASKET_B = (275.09, 240.00, 44.05)  # Vehicle basket
+HOME_POS = (203.27, 10.36, -14.22, 0)  # Home position
 
 #PORT
 PORT = "/dev/ttyACM0"
@@ -43,59 +42,52 @@ dobot.move_to(*HOME_POS)
 print("At home position, starting continuous sorting...")
 
 try:
-    cycle = 1
     # ---- Camera ON ----
-    cap = cv2.VideoCapture("/dev/video2")
+    # cap = find_camera()
     cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
     cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
-
-    print(f"\n=== Cycle {cycle} ===")
-
     print("Camera feed ON")
-
-    print("Capturing for 3 seconds...")
-
-
+    cycle = 1
     while True:
-    
+        print(f"\n=== Cycle {cycle} ===")
+
+
+        print("Capturing for 3 seconds...")
         frames = []
         start = time.time()
-        frames = []
-        while time.time() - start < 3:
+        while time.time() - start < 5:
             ret, frame = cap.read()
-            frames.append(frame)
             if not ret:
                 continue
-        
-        print(len(frames))        
 
-        if frames != []:
+            # Run YOLO inference
+            results = model(frame, verbose=False)[0]
+            detected_objects = set()
+            # Draw detections
+            if results.boxes is not None:
+                for box, conf, cls in zip(results.boxes.xyxy, results.boxes.conf, results.boxes.cls):
+                    if conf >= 0.90:
+                        x1, y1, x2, y2 = map(int, box)
+                        obj_name = results.names[int(cls)]
+                        label = f"{obj_name} {conf:.2f}"
+                        print("detected_object", label, VEHICLE, x1,y1,x2,y2, label.strip() in VEHICLE)
+                        color = (0, 255, 0) if obj_name in FOOD else (0, 0, 255)
+                        cv2.rectangle(frame, (x1, y1), (x2, y2), color, 2)
+                        cv2.putText(frame, label, (x1, y1 - 10),
+                                    cv2.FONT_HERSHEY_SIMPLEX, 0.6, color, 2)
 
-            for frame in frames:
-                # Run YOLO inference
-                print(frame.shape)
-                results = model(frame, verbose=False)[0]
-                detected_objects = set()
-                # Draw detections
-                if results.boxes is not None:
-                    for box, conf, cls in zip(results.boxes.xyxy, results.boxes.conf, results.boxes.cls):
-                        if conf >= 0.90:
-                            x1, y1, x2, y2 = map(int, box)
-                            obj_name = results.names[int(cls)]
-                            label = f"{obj_name} {conf:.2f}"
-                            color = (0, 255, 0) if obj_name in FOOD else (0, 0, 255)
-                            cv2.rectangle(frame, (x1, y1), (x2, y2), color, 2)
-                            cv2.putText(frame, label, (x1, y1 - 10),
-                                        cv2.FONT_HERSHEY_SIMPLEX, 0.6, color, 2)
-                            
-                            if obj_name in FOOD or obj_name in VEHICLE:
-                                detected_objects.add(obj_name)
+                        
+                        if obj_name in FOOD or obj_name in VEHICLE:
+                            print("adding", obj_name)
+                            detected_objects.add(obj_name)
+                        
+                        
 
-                    frames.append(frame)
+            frames.append(frame)
 
-                cv2.imshow("Camera Feed", frame)
-                if cv2.waitKey(1) & 0xFF == ord('q'):
-                        raise KeyboardInterrupt
+            cv2.imshow("Camera Feed", frame)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                raise KeyboardInterrupt
 
         print(f"Captured {len(frames)} frames")
 
@@ -117,20 +109,18 @@ try:
                 dobot.move_to(PICKUP_POS[0], PICKUP_POS[1], PICKUP_POS[2] + 20, 0)
                 dobot.move_to(*PICKUP_POS, 0)
                 dobot.suck(True)
-                time.sleep(2)
                 dobot.move_to(PICKUP_POS[0], PICKUP_POS[1], PICKUP_POS[2] + 20, 0)
 
                 # Destination
                 if obj in FOOD:
                     basket = BASKET_A
                     print(f"→ FOOD detected: {obj} → Basket A")
-                elif obj in VEHICLE: 
+                elif obj in VEHICLE:
                     basket = BASKET_B
                     print(f"→ VEHICLE detected: {obj} → Basket B")
                 else:
                     print(f"→ UNKNOWN category: {obj}, dropping here")
                     dobot.suck(False)
-                    time.sleep(2)
                     continue
 
                 # Place in basket
@@ -158,5 +148,6 @@ finally:
     except:
         pass
     cv2.destroyAllWindows()
+    dobot.move_to(*HOME_POS)
     dobot.close()
     print("Cleanup complete!")
